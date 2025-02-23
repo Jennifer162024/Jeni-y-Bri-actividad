b53f95390463824691ddb13e4601dca30b4c67be
Jennifer162024 <jennifer.martinez2026@hilasal.superate.org.sv>
2025-02-22 18:41:50 -0600

    first commi

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-document.write('Manual de Prácticas de Estudio para Informática')
+document.write('<ul>');
+document.write('<li>Manual de Prácticas de Estudio para Informática</li>');
+document.write('<li>Estrategias para aprender a programar:</li>');
+document.write('<ul>');
+document.write('<li>Hacer preguntas si no entendiste un tema</li>');
+document.write('<li>No temas a los errores</li>');
+document.write('<li>Se resiliente</li>')
+document.write('<li>Organiza tu espacio de trabajo</li>');
+document.write('<li>No temas pedir ayuda</li>')
+document.write('</ul>'); 
+
+document.write('<li>Consejos:</li>');
+document.write('<ul>');
+document.write('<li>Cumplir con las tareas</li>');
+document.write('<li>Tomar apuntes</li>');
+document.write('<li>Mantén la calma con los proyectos</li>');
+document.write('<li>No faltar a clases</li>');
+document.write('<li>Aprende de tus errores</li>');
+document.write('</ul>'); 
+
+document.write('<li>Técnicas:</li>');
+document.write('<ul>');
+document.write('<li>Trabaja en equipo</li>');
+document.write('<li>Practica constatemente</li>');
+document.write('<li>Establecer un horario para resolver al menos un problema diario, esto ayudará a mejorar tu lógica y habilidades de resolución de problemas</li>');
+document.write('</ul>');  
+
+document.write('</ul>');  
+s
